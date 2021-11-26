12a10b24e137a8f20a51a2e60fac4f0ebe270945
Khoadoanduy <89672465+Khoadoanduy@users.noreply.github.com>
2021-11-26 21:07:48 +0700

    Tạo Home.js

--- Home.js
+++ Home.js
@@ -9,14 +9,3 @@ let circle = document.querySelector(".color-option");
         document.querySelector(`.main-images .${target.id}`).classList.add("active");
       }
     });
-let circle = document.querySelector(".color-option1");
- 
-    circle.addEventListener("click", (e)=>{
-      let target = e.target;
-      if(target.classList.contains("circle1")){
-        circle.querySelector(".active").classList.remove("active");
-        target.classList.add("active");
-        document.querySelector(".main-images .active1").classList.remove("active1");
-        document.querySelector(`.main-images .${target.id}`).classList.add("active1");
-      }
-    });
